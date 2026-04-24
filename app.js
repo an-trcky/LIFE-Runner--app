1ba41a3b0c115936e7e84fa3253f6e640361152c
an-trcky <f2krat0202@gmail.com>
2026-04-24 13:38:14 +0900

    Add function to show specific screen

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+function showScreen(name){
+  document.getElementById("screen-home").style.display = "none"
+  document.getElementById("screen-input").style.display = "none"
+  document.getElementById("screen-result").style.display = "none"
 
+  document.getElementById("screen-" + name).style.display = "block"
+}
