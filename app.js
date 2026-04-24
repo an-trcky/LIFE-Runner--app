f1e2e3fe6a91f8fede0a3316938cbd4cc317378c
an-trcky <f2krat0202@gmail.com>
2026-04-24 14:09:08 +0900

    Refactor showScreen and add event listener on load
    Refactor showScreen function and initialize event listener on DOMContentLoaded.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-function showScreen(name){
-  document.getElementById("screen-home").style.display = "none"
-  document.getElementById("screen-input").style.display = "none"
-  document.getElementById("screen-result").style.display = "none"
+console.log("JS OK")
 
-  document.getElementById("screen-" + name).style.display = "block"
-}
-　document.getElementById("startBtn").addEventListener("click",()=>{
-  alert("ここから診断画面へ（次で作る）")
+window.addEventListener("DOMContentLoaded", () => {
+
+  const startBtn = document.getElementById("startBtn")
+
+  if(!startBtn){
+    console.log("ボタン取得失敗")
+    return
+  }
+
+  startBtn.addEventListener("click", () => {
+    alert("診断画面へ（次で実装）")
+  })
+
 })
