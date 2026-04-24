9d8aa21263116b814728f5c77a99f249fd173985
an-trcky <f2krat0202@gmail.com>
2026-04-24 16:47:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 document.getElementById("startBtn").addEventListener("click",()=>{
 
-  const age = document.getElementById("age").value
-  const savings = document.getElementById("savings").value
-  const income = document.getElementById("income").value
-  const expenses = document.getElementById("expenses").value
+  const age = Number(document.getElementById("age").value)
+  const savings = Number(document.getElementById("savings").value)
+  const income = Number(document.getElementById("income").value)
+  const expenses = Number(document.getElementById("expenses").value)
 
-  if(!age || !savings || !income || !expenses){
-    alert("全部入力して")
-    return
-  }
-
-  // 仮処理（あとでシミュレーションに繋ぐ）
-  alert("診断スタート！（ここからシミュレーションへ）")
+  console.log(age, savings, income, expenses)
 
 })
