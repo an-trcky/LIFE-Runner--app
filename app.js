209998e5fab6fb29aee691abfc44291ed76bf918
an-trcky <f2krat0202@gmail.com>
2026-04-24 16:57:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ document.getElementById("startBtn").addEventListener("click",()=>{
   const expenses = Number(document.getElementById("expenses").value)
 
   let yearly = (income - expenses) * 12
-  let future = savings + yearly * 10
+  let history = []
+
+let cash = savings
+
+for(let i=0;i<10;i++){
+  cash += yearly
+  history.push(cash)
+}
   
   document.getElementById("resultBox").innerHTML =
     "10年後資産：" + future + "万円"
